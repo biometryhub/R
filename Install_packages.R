a915919b41a7a001cd780e84114254ffd15f7229
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2020-02-14 14:44:05 +1030

    Updated to correctly detect installed packages

--- Install_packages.R
+++ Install_packages.R
@@ -1,51 +1,51 @@
 required <- c(
-	# tidyverse
-	"tidyverse",
-	# plotting
-	"ggthemes",
-	# Rstudio
-	"knitr", "rmarkdown", 
-	# Experimental design
-	"agricolae",
-	#W2 extra packages
-	"emmeans", "asremlPlus", "gridExtra", "ggpubr", "data.table",
-	#W3 packages
-	"tinytex", "pander", "kableExtra", "xtable", "devtools"
+  # tidyverse
+  "tidyverse",
+  # plotting
+  "ggthemes",
+  # Rstudio
+  "knitr", "rmarkdown", 
+  # Experimental design
+  "agricolae",
+  #W2 extra packages
+  "emmeans", "asremlPlus", "gridExtra", "ggpubr", "data.table",
+  #W3 packages
+  "tinytex", "pander", "kableExtra", "xtable", "devtools"
 )
 installed <- rownames(installed.packages())
 notInstalled <- setdiff(required, installed)
 
 if (length(notInstalled) > 0){
-    
-    message("Installing required packages...\n")
-    install.packages(notInstalled, quiet = TRUE, type = "binary")
-    
-    installed <- rownames(installed.packages()) # Update to see what's there now
-    missingPackages <- setdiff(required, installed)
-    
-    
-    if (length(missingPackages) > 0){
-        invisible(sapply(missingPackages, 
-                         function(x){
-                             warning("\nThe package ", x, " has not installed sucessfully.\n",
-                                     "Please copy and paste the code:\n\n\t install.packages('", x,
-                                     "')\n\n and try running it again on the console. \n\n
-					If that fails again, please email the output to s.rogers@adelaide.edu.au\n")
-                         }))
-        stop("Installation unsuccessful.")
-    }
-    else{
-        message("\nAll required packages have been installed.\n\n\n")
-    }
+  
+  message("Installing required packages...\n")
+  install.packages(notInstalled, quiet = TRUE, type = "binary")
+  
+  installed <- rownames(installed.packages()) # Update to see what's there now
+  missingPackages <- setdiff(required, installed)
+  
+  
+  if (length(missingPackages) > 0){
+    invisible(sapply(missingPackages, 
+                     function(x){
+                       warning("\nThe package ", x, " has not installed sucessfully.\n",
+                               "Please copy and paste the code:\n\n\t install.packages('", x,
+                               "')\n\n and try running it again on the console. \n\n
+                               If that fails again, please email the output to s.rogers@adelaide.edu.au\n")
+                     }))
+    stop("Installation unsuccessful.")
+                     }
+  else{
+    message("\nAll required packages have been installed.\n\n\n")
+  }
 }
 
-devtools::install_github("biometryhub/BiometryTraining", upgrade = "never", quiet = TRUE)
+devtools::install_github("biometryhub/BiometryTraining", upgrade = "never", quiet = TRUE, )
 
-if(!"BiometryTraining" %in% installed) {
-	warning("\nThe BiometryTraining package has not installed sucessfully.\n",
-		"Please copy and paste the code:\n\n\t devtools::install_github('biometryhub/BiometryTraining', upgrade = 'never')\n\n 
-		and try running it again on the console. \n\n
-		If that fails again, please email the output to s.rogers@adelaide.edu.au\n")
+if(!"BiometryTraining" %in% rownames(installed.packages())) {
+  warning("\nThe BiometryTraining package has not installed sucessfully.\n",
+          "Please copy and paste the code:\n\n\t devtools::install_github('biometryhub/BiometryTraining', upgrade = 'never')\n\n 
+          and try running it again on the console. \n\n
+          If that fails again, please email the output to s.rogers@adelaide.edu.au\n")
 }
 
 #Clean up variables
@@ -62,6 +62,3 @@ if(!"BiometryTraining" %in% installed) {
 #source("https://raw.githubusercontent.com/rogerssam/R/master/W2_functions.r")
 #OR source("http://bit.do/W2_functions")
 save.image()
-
-
-
