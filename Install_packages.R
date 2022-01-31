6511aa643ce0b8a71f1b285604c51c8a0b758d2a
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2022-01-31 12:52:25 +1030

    Update to use Ncpus

--- Install_packages.R
+++ Install_packages.R
@@ -14,7 +14,8 @@ notInstalled <- setdiff(required, installed)
 if (length(notInstalled) > 0){
   
   message("Installing required packages...\n")
-  install.packages(notInstalled, quiet = TRUE, type = ifelse(Sys.info()[['sysname']]=="Linux", "source", "binary"))
+  install.packages(notInstalled, quiet = TRUE, type = ifelse(Sys.info()[['sysname']]=="Linux", "source", "binary"), 
+                  Ncpus = getOption("Ncpus", max(parallel::detectCores() - 2, 1)))
   
   installed <- rownames(installed.packages()) # Update to see what's there now
   missingPackages <- setdiff(required, installed)
@@ -26,7 +27,7 @@ if (length(notInstalled) > 0){
                        warning("\nThe package ", x, " has not installed sucessfully.\n",
                                "Please copy and paste the code:\n\n\t install.packages('", x,
                                "')\n\n and try running it again on the console. \n\n
-                               If that fails again, please email the output to sam.rogers@adelaide.edu.au\n")
+                               If that fails again, please email the output to biometrytraining@adelaide.edu.au\n")
                      }))
     stop("Installation unsuccessful.")
                      }
